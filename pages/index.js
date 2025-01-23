14617050bca9df34da30fc3a49c15f034a7c629b
GenuineJack <6517070+GenuineJack@users.noreply.github.com>
2025-01-23 11:09:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -240,16 +240,16 @@ export default function Home() {
         }
         
         .modal {
-        position: fixed;
-        z-index: 100;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0,0,0,0.5);
-        display: flex;
-        justify-content: center;
-        align-items: center;
+          position: fixed;
+          z-index: 100;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: rgba(0,0,0,0.5);
+          display: flex;
+          justify-content: center;
+          align-items: center;
       }
       
       .modal-content {
@@ -259,6 +259,10 @@ export default function Home() {
         max-width: 500px;
         width: 90%;
         position: relative;
+        margin: auto;
+        top: 50%;
+        transform: translateY(-50%);
+        color: black;
       }
       
       .close {
