0a1883ba27e5b1d94c6c8bdc02f12780f57456f7
GenuineJack <6517070+GenuineJack@users.noreply.github.com>
2025-02-25 11:52:42 -0500

    Update and rename index.js to index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home(): JSX.Element {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Welcome to Stumble Higher</h1>
+      <p>This is your homepage. Edit this file to add content.</p>
+    </div>
+  );
+}
